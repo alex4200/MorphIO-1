03ebd504b5fd03155d24d2d2354e47c144646fff
Mike Gevaert <michael.gevaert@epfl.ch>
2021-02-25 08:28:33 +0100

    windows build

--- setup.py
+++ setup.py
@@ -22,32 +22,6 @@ class CMakeExtension(Extension):
         self.sourcedir = os.path.abspath(sourcedir)
 
 
-def find_cmake():
-    for candidate in ['cmake', 'cmake3']:
-        try:
-            out = subprocess.check_output([candidate, '--version'])
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
-                                                   out.decode()).group(1))
-            if cmake_version >= '3.2.0':
-                return candidate
-        except OSError:
-            pass
-
-    raise RuntimeError("CMake >= 3.2.0 must be installed to build MorphIO")
-
-
-def get_cpu_count():
-    try:
-        return len(os.sched_getaffinity(0))  # linux only
-    except:
-        pass
-
-    try:
-        return os.cpu_count()  # python 3.4+
-    except:
-        return 1  # default
-
-
 class CMakeBuild(build_ext):
     user_options = build_ext.user_options + [
         ("cmake-defs=", None, "Additional CMake definitions, comma split")
@@ -58,11 +32,10 @@ class CMakeBuild(build_ext):
         self.cmake_defs = None
 
     def run(self):
-        cmake = find_cmake()
         for ext in self.extensions:
-            self.build_extension(ext, cmake)
+            self.build_extension(ext)
 
-    def build_extension(self, ext, cmake):
+    def build_extension(self, ext):
         extdir = os.path.abspath(
             os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
@@ -78,27 +51,34 @@ class CMakeBuild(build_ext):
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
 
-        if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
-                cfg.upper(),
-                extdir)]
-            cmake_args += ['-G', os.getenv('MORPHIO_CMAKE_GENERATOR',
-                                           "Visual Studio 15 2017 Win64")]
-        else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE={}'.format(cfg),
-                           '-DMorphIO_CXX_WARNINGS=OFF',
-                           ]
-            build_args += ["--", "-j{}".format(max(MIN_CPU_CORES, get_cpu_count())),
-                           ]
+        #if platform.system() == "Windows":
+        #    cmake_args += ['-G', os.getenv('MORPHIO_CMAKE_GENERATOR', 'Ninja'),
+        #                   ]
+
+        cmake_args += ['-DCMAKE_BUILD_TYPE={}'.format(cfg),
+                       '-DMorphIO_CXX_WARNINGS=OFF',
+                       '-GNinja',
+                       ]
 
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
 
-        subprocess.check_call([cmake, ext.sourcedir] + cmake_args, cwd=self.build_temp)
-        subprocess.check_call([cmake, '--build', '.', '--target', '_morphio'] + build_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--trace', '--trace-expand', '--debug-find', ext.sourcedir] + cmake_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--build', '.', '--target', '_morphio'] + build_args, cwd=self.build_temp)
 
-with open('README.md') as f:
-    long_description = f.read()
+#XXX
+long_description = 'f.read()'
+
+setup_requires = ['cmake',
+                  'ninja',
+                  'setuptools_scm',
+                  ]
+install_requires = ['numpy>=1.14.1',
+                    ]
+
+if platform.system() == 'Windows':
+    install_requires += ['h5py',   # use h5py's hdf5 install
+                         ]
 
 setup(
     name='MorphIO',
@@ -106,7 +86,7 @@ setup(
     description='A neuron morphology IO library',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=['numpy>=1.14.1'],
+    install_requires=install_requires,
     url='https://github.com/BlueBrain/MorphIO/',
     ext_modules=[CMakeExtension('morphio._morphio')],
     cmdclass=dict(build_ext=CMakeBuild),
@@ -122,5 +102,5 @@ setup(
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
     ],
     use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    setup_requires=setup_requires,
 )
